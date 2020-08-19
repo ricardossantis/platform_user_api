380521d6eb9e908679eac7f8c9bfd24cafcf9f0f
Ricardo <rsdsantis@gmail.com>
2020-08-19 20:25:42 -0300

    Feat: basic structure authenticator

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRoute } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRoute>
+    <BrowserRouter>
       <App />
-    </BrowserRoute>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
