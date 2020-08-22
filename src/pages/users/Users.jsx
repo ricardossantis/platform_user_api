20cfdb1c95d921a17d280f1b418e32b3827d9cc7
Ricardo Santis <rsdsantis@gmail.com>
2020-08-22 21:56:45 +0000

    Filterusers

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import { BackGround, StyledDiv, StyledPagination } from "./Users.js";
 import Card from "../../components/card-users/Card.jsx";
 import Api from "../../services/api.js";
+import { Input } from "antd";
+const { Search } = Input;
 
 function Users() {
-  let numsItems = Array(12).fill(0);
+  const [numItems, setNumItems] = useState(Array(12).fill(0));
   const [users, setUsers] = useState(Array(50).fill(""));
+  const [totalUsers, setTotalUsers] = useState();
   const [pagination, setPagination] = useState(0);
+  const [pageSize, setSize] = useState(12);
   let counter = pagination;
 
   useEffect(() => {
@@ -14,34 +18,60 @@ function Users() {
     Api.get(`users`, {
       headers: { Authorization: token },
     }).then((res) => {
-      console.log(res);
       setUsers(res.data);
-    }, []);
-  });
+      setTotalUsers(res.data);
+    });
+  }, []);
 
   const handlePagination = (page) => {
-    setPagination(page * 12);
+    setPagination(page * pageSize);
+  };
+
+  const handleSearch = (value) => {
+    let filtered = totalUsers.filter(
+      (item) => item.name !== null && item.name.match(value)
+    );
+    setUsers(filtered);
+    setNumItems(Array(filtered.length).fill(0));
+  };
+
+  const handlePageSize = (page, size) => {
+    setNumItems(Array(size).fill(0));
+    setSize(size);
   };
 
   return (
     <BackGround>
+      <Search
+        placeholder="Insert user name"
+        onSearch={handleSearch}
+        enterButton
+      />
       <StyledDiv>
-        {numsItems.map((item, indexItem) => {
+        {numItems.map((item, indexItem) => {
           counter += 1;
+          console.log(users);
           return (
-            <Card
-              key={indexItem}
-              name={users[counter - 1].name}
-              user={users[counter - 1].user}
-              email={users[counter - 1].email}
-            />
+            counter <= users.length && (
+              <Card
+                key={indexItem}
+                name={users[counter - 1].name}
+                user={users[counter - 1].user}
+                email={users[counter - 1].email}
+              />
+            )
           );
         })}
       </StyledDiv>
       <StyledPagination
         defaultCurrent={1}
         onChange={handlePagination}
-        total={users.length / 12}
+        total={users.length}
+        showTotal={(total, range) =>
+          `${range[0]}-${range[1]} of ${total} items`
+        }
+        defaultPageSize={12}
+        onShowSizeChange={handlePageSize}
       />
     </BackGround>
   );
